92c1977c875a8fc4cb3f9d3f0dd460bf5bac5a7a
JN-Jones <Jones_h1@web.de>
2014-11-02 20:57:24 +0100

    Remove wrong copyright

--- inc/plugins/mybb2fa.php
+++ inc/plugins/mybb2fa.php
@@ -1,12 +1,4 @@
 <?php
-/**
- * MyBB 1.8 Merge System
- * Copyright 2014 MyBB Group, All Rights Reserved
- *
- * Website: http://www.mybb.com
- * License: http://www.mybb.com/download/merge-system/license/
- */
-
 // Disallow direct access to this file for security reasons
 if(!defined("IN_MYBB"))
 {
