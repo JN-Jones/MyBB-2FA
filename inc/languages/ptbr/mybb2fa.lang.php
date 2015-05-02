642968d78434a7969c1fa87ad3608224dc3268a0
ArnoldLayne-xXx <dthiago@users.noreply.github.com>
2015-05-02 02:47:07 -0300

    Create mybb2fa.lang.php

---
+++ inc/languages/ptbr/mybb2fa.lang.php
@@ -0,0 +1,11 @@
+<?php
+$l['mybb2fa_activated'] = "Autenticação em 2-Fatores Ativado.Por favor escaneie com o seu leito de QR Code.";
+$l['mybb2fa_loggedin'] = "Código Coreto! Você está dentro!";
+$l['mybb2fa_failed'] = "Putz!Código incorreto.Será deslogado";
+$l['mybb2fa'] = "Autenticação em 2 fatores (2 passos)";
+$l['mybb2fa_code'] = "Insira o código de Autenticação";
+$l['mybb2fa_check'] = "Verifique";
+$l['mybb2fa_activated_desc'] = "Autenticação em 2 fatores ativado. Escaneie o código QR abaixo para certificar que possa logar em seu fórum.";
+$l['mybb2fa_deactivate'] = "Desativado";
+$l['mybb2fa_deactivated_desc'] = "Autenticação em 2 fatores desativado. Você precisa ter um programa de autenticação como o Google Autenticador instalado.";
+$l['mybb2fa_activate'] = "Ativado";
